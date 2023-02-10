076d9357bdecae7b9456842f6f4260264f39f768
Roberto Plancarte <roberto.plancarte@apptegy.com>
2023-02-09 18:56:01 -0600

    Initial commit

---
+++ lib/apptegy_feature_flags.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "apptegy_feature_flags/version"
+
+module ApptegyFeatureFlags
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/apptegy_feature_flags_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ApptegyFeatureFlags do
+  it "has a version number" do
+    expect(ApptegyFeatureFlags::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
