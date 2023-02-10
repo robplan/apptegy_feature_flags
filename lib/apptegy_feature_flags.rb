23122ce4338796f9003f3af055fd13d55b622807
Roberto Plancarte <roberto.plancarte@apptegy.com>
2023-02-10 09:52:08 -0600

    Wrap Flipper into Apptegy::FeatureFlags

--- lib/apptegy_feature_flags.rb
+++ lib/apptegy_feature_flags.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
+require "flipper-cloud"
 require_relative "apptegy_feature_flags/version"
 
 module ApptegyFeatureFlags
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < StandardError
+  end
 end
+
+# Top level namespace for all Apptegy gems
+module Apptegy
+  FEATURE_FLAGS_TOKEN = ENV.fetch("APPTEGY_FEATURE_FLAGS_TOKEN", "missing_feature_flags_token")
+  FeatureFlags = Flipper
+
+  Flipper.configure do |config|
+    config.default do
+      Flipper::Cloud.new({ token: FEATURE_FLAGS_TOKEN })
+    end
+  end
+end
+
+Apptegy::FeatureFlags.features
